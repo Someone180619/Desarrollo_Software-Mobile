17d04e23f042d61904a9997dab4c1c31231b3a2f
Someone180619 <51931813+Someone180619@users.noreply.github.com>
2022-06-14 10:59:50 -0500

    [TALLER 25] Agregado

---
+++ TALLER_N25_VIDEO/app/src/main/java/com/example/taller_n25_video/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.taller_n25_video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.crabrave;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
+    }
+}
